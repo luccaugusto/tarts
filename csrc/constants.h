ba46cce95664c605a3e36a8c24ce61ea992a8f14
lrr68 <lucca@luccaaugusto.xyz>
2021-09-19 11:43:07 -0300

    refactor constants

--- csrc/constants.h
+++ csrc/constants.h
@@ -1,13 +1,21 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define FRAMED 1
+#define FRAMED 0
 #define HEIGHT 30
 #define WIDTH 124
-#define FRAME_CORNER '+'
-#define FRAME_BORDER_H '-'
-#define FRAME_BORDER_V '|'
 #define BLANK ' '
 #define MAX_NAME_LENGTH 50
+
+enum Frame {
+	FRAME_CORNER   = '+',
+	FRAME_BORDER_H = '-',
+	FRAME_BORDER_V = '|',
+};
+
+enum Pixel {
+	BACK = 0,
+	FORE = 1,
+};
 
 #endif
